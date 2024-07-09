22ca0fe4fd0d1cd364c1dff85ac9348e6c6c3b19
leandrofabianjr <leandrofabianjr@gmail.com>
2024-07-09 19:15:01 -0300

    Iniciando aplicação

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Render } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root(@Res() res: Response) {
+    return { 
+      meta: {
+        title: 'Login'
+      }
+     };
   }
 }
